18e8227565cdd07fd49e4ae2b2afb8a590bd4d89
Shafiaks <113788543+Shafiaks@users.noreply.github.com>
2023-01-18 13:16:13 +0100

    showing content from db, added changed pass, update

--- controllers/spaceContollers.js
+++ controllers/spaceContollers.js
@@ -4,20 +4,16 @@ const spaces = require('../SpaceData.json');
 
 
 const getAllSpaces = async (req, res) => {
-	// try {
-	// 	console.log("pool connected");
-	// 	const { rows } = await pool.query('SELECT * FROM Spaces;');
-	// 	pool
-	// 		.connect()
-	// 		.then(() => {
-	// 			pool.query('SELECT * FROM Spaces;')
-	// 				.then(response => console.log(response.rows))
-	// 				.catch(err => console.log("error inside pool", err))
-	// 		})
-	// } catch (error) {
-	// 	console.log("ERRRRRR  -- ", error.message);
-	// }
-	return res.send(spaces);
+	try {
+		console.log("pool connected");
+		const { rows } = await pool.query('SELECT * FROM Spaces;');
+
+		if(rows.length===0) return res.status(404).send("No space found !")
+		return res.send(rows);
+	} catch (error) {
+		console.log("ERRRRRR  -- ", error.message);
+	}
+	//return res.send(spaces);
 };
 
 
@@ -37,8 +33,8 @@ const createSpace = async (req, res) => {
 		// .connect()
 		// .then(() => {
 			pool.query(`INSERT INTO Spaces(
-				title,area,ownerName, ownerEmail, costperDay, maxPeople, description, address,city,state, zip,country,is_available,imgUrl)
-				VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14);`, [title, area,ownerName, ownerEmail, costperDay, maxPeople, description, address,city,state, zip,country,"true",spacePicUrl])
+				title,area,ownerName, ownerEmail, costperDay, maxPeople, description, address,city,state, zip,country,is_available,imgUrl,openhours_from,openhours_to,opendays_from,opendays_to)
+				VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18);`, [title, area,ownerName, ownerEmail, costperDay, maxPeople, description, address,city,state, zip,country,"true",spacePicUrl,"07","19","Monday "," Saturday"])
 				.then(response => {
 					console.log(response);
 					return res.status(201).send('Success');
